f96e1426dcba6a94f4ad4832cc6f367a81ba4ad9
Pearly Ferguson <pearly@Pearlys-MacBook-Air.local>
2020-02-21 17:10:35 +1300

    managed to retrieve data from newsapi

--- js/script.js
+++ js/script.js
@@ -11,17 +11,38 @@ $(document).ready(function(){
   myKey = myKey[0].key;
   console.log(myKey);
 
+
+document.getElementById('submit').addEventListener('click', function(){
+
+  var country = document.getElementById('country').value;
+  console.log(country);
+});
+
+
+
   $.ajax({
     // url : `http://newsapi.org/v2/top-headlines?country=us&apiKey=1a9818ada3194ab28335127795b6d9e3`,
     url : `http://newsapi.org/v2/top-headlines?country=us&apiKey=${myKey}`,
-    type : 'json',
+    type : 'GET',
+    data : 'json',
     success : function(data){
       console.log(data);
 
       var i;
-      for (i = 0; i < data.article.length, i++){
+      for (i = 0; i < data.articles.length; i++) {
+
         document.getElementById('result').innerHTML +=
-         '<p>ID: ' + data.article[i].source.id + '</p>'
+        '<div class="col col-sm-6 col-md-3 col-lg-3 my-3">' +
+          '<div class="card" style="width: 18rem;">' +
+            '<img class="card-img-top" src="' + data.articles[i].urlToImage + '" alt="image"></img>' +
+            '<div class="card-body">' +
+                '<h4>' + data.articles[i].title + '</h4>' +
+                '<p>' + data.articles[i].description + '</p>' +
+                '<p>Author: ' + data.articles[i].author + '</p>' +
+                '<a class="card-link" href=" ' + data.articles[i].url + '">Read more..</a>' +
+            '</div>' +
+          '</div>' +
+        '</div>'
       }
     },
 
